3e6cfdd8558519a88aabf0efbda0ac9373d4b73c
Bojan Stankovic <15022633@students.southwales.ac.uk>
2018-12-13 17:18:19 +0000

    Updated notes, Made bot only to walk toward player

--- python-bot/python_doommarine.py
+++ python-bot/python_doommarine.py
@@ -145,27 +145,27 @@ def botMain (b):
     you = findYou (b)
 
     while True:
-        # moveTowards (you)
-        # b.face (you)
+        moveTowards (you)
+        b.face (you)
         print b.get_health ()
-        b.change_weapon (3)
-        print "changed to 3"
-        time.sleep (2)
-        b.start_firing ()
-        time.sleep (2)
-        print "fired"
+        # b.change_weapon (3)
+        # print "changed to 3"
+        # time.sleep (2)
+        # b.start_firing ()
+        # time.sleep (2)
+        # print "fired"
         # b.change_weapon (0)
         # time.sleep (10)
-        b.change_weapon (1)
-        print "changed to 1"
-        b.start_firing ()
-        print "fired"
+        # b.change_weapon (1)
+        # print "changed to 1"
+        # b.start_firing ()
+        # print "fired"
         # time.sleep (10)
         # b.change_weapon (3)
-        time.sleep (2)
-        b.stop_firing ()
-        print "stopped firing"
-        time.sleep (2)
+        # time.sleep (2)
+        # b.stop_firing ()
+        # print "stopped firing"
+        time.sleep (1)
 
 
 
